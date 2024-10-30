94f8293929e68b8eb6d2ffa6315514e595946ba5
Renato Figueiredo Dias Santos <renatofigueiredo257@gmail.com>
2024-10-30 17:56:29 -0400

    Add files via upload

---
+++ 24.10(inc. python)/30.10.py
@@ -0,0 +1,16 @@
+y = 3
+x = y+1
+
+if 5 > 2:
+    print("5 is bigger than 2")
+    
+else:
+    print("5 is not bigger than 2")
+
+
+for x, i, y in range(5):
+    i = 0
+    i = i + x
+    y = 3
+    x = y+1
+print(i)
---
+++ 24.10(inc. python)/index.py
@@ -0,0 +1,12 @@
+print('Hello world')
+
+x = str(3)
+y = int(3)
+z = float(3.1)
+x = x + x
+y= y + y
+z = z + z
+
+print(x)
+print(y)
+print(z)
